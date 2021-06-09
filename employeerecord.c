4268324a64f7384dbec2d6eb6be2b77426e6fbce
Monica <monicabee358@gmail.com>
2021-06-09 23:30:35 +0800

    caps

--- employeerecord.c
+++ employeerecord.c
@@ -10,6 +10,8 @@ struct employeeRec{
         int salaryLevel;
 };
 void getEmployeeInfo(struct employeeRec emp);
+char* convertToUpperCase(char arr[]);
+
 
 int main()
 {
@@ -23,8 +25,12 @@ int main()
 void getEmployeeInfo(struct employeeRec emp){
 	int choice,ch,c,flag=0;
 	FILE *fp;
+	char* searchCode;
 	char scode[20];
 	float salaryRate;
+	
+	char s[100] = "a01-0001";
+   int i;
 	do 
 		{
 		 printf("\nEmployee Info\n");
@@ -37,7 +43,7 @@ void getEmployeeInfo(struct employeeRec emp){
 		 switch (choice)
 		 {
 		     case 1: 
-		      	 fp=fopen("employee.txt","a");
+		      	 fp=fopen("employee.txt","ab");
 		      	 	system("cls");
                     printf("Enter the Employee Code:\n");
                     scanf("%s",emp.employeeCode);
@@ -47,11 +53,6 @@ void getEmployeeInfo(struct employeeRec emp){
                     fflush(stdin);
                     printf("Enter the Salary Level:\n");
                     scanf("%d", &emp.salaryLevel);
-//                    fwrite(&emp,sizeof(emp),1,fp);
-//                    fflush(stdin);
-//                    printf("Enter the Salary Rate:\n");
-//                    scanf("%d", &emp.salaryRate);
-//                    printf("%d", emp.salaryRate);
                     fwrite(&emp,sizeof(emp),1,fp);
                     fflush(stdin);
                     fclose(fp);
@@ -59,13 +60,14 @@ void getEmployeeInfo(struct employeeRec emp){
                     system("cls");
                     break;
 		     case 2:
-		     		fp=fopen("employee.txt","r");
+		     		fp=fopen("employee.txt","rb");
 					system("cls");
                     printf("Enter the Employee Code\n");
                     scanf("%s",scode);
+                    searchCode=convertToUpperCase(scode);
                     while(fread(&emp,sizeof(emp),1,fp))
                     {
-                        if(strcmp(scode,emp.employeeCode)==0)
+                        if(strcmp(searchCode,emp.employeeCode)==0)
                         {
                             printf("Employee Name: %s\n",emp.employeeCode);
                             printf("Employee Code: %s\n",emp.employeename);
@@ -99,6 +101,22 @@ void getEmployeeInfo(struct employeeRec emp){
 		 } 
 		} while (choice != 3);
 
+}
+
+char* convertToUpperCase(char arr[]){
+	char *retString = malloc (sizeof (char) * 256);
+	int i;
+	for (i = 0; arr[i]!='\0'; i++) {
+		
+      if(arr[i] >= 'a' && arr[i] <= 'z') {
+         retString[i] = arr[i] -32;
+      }else{
+      	retString[i] = arr[i];
+	  }
+      
+   }
+   retString[i]='\0';
+   return retString;
 }
 
 
